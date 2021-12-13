68b7327cbc277f0ce8744eb4a17c8fb68914f8f8
Maxence Petitpierre <maxence.petitpierre@vadesecure.com>
2021-12-13 22:20:20 +0100

    Add NSQ producer

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # effective-bassoon
+
+## Objective
+
+I want to try a simple implementation of NSQ in Golang.
---
+++ producer.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/nsqio/go-nsq"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	localhost string = "127.0.0.1"
+	port      string = "4150"
+
+	topic string = "effective_bassoon"
+)
+
+type Message struct {
+	Name      string
+	Content   string
+	Timestamp int64
+}
+
+func main() {
+	nsqConf := nsq.NewConfig()
+
+	producer, err := nsq.NewProducer(localhost+":"+port, nsqConf)
+	if err != nil {
+		log.Fatal().Err(err).Msg("NSQ Producer could not connect")
+	}
+
+	msg := Message{
+		Name:      "Message Name",
+		Content:   "Message Content",
+		Timestamp: time.Now().Unix(),
+	}
+
+	payload, err := json.Marshal(msg)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Message marshalling error")
+	}
+
+	err = producer.Publish(topic, payload)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Publish message error")
+	}
+}
